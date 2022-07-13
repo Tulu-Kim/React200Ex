0f454c17020f828e2f3a30d73acc4315ec52ef0b
TOOL-Kim <hansol318213@gmai.com>
2022-07-14 08:37:33 +0900

    11까지함

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import R011_SpreadOperator from "./Es6/R011_SpreadOperator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <R011_SpreadOperator />
     </div>
   );
 }
